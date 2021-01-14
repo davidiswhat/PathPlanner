81640a2ea228aba8a9162ad350860fdb3c6626f3
davidiswhat <david.li@ufl.edu>
2021-01-14 06:23:22 -0500

    Add files via upload

---
+++ PathSearch/PathSearch.cpp
@@ -0,0 +1,295 @@
+#pragma once
+#include "PathSearch.h"
+
+
+namespace ufl_cap4053
+{
+	namespace searches
+	{
+		// CLASS DEFINITION GOES HERE
+		DLLEXPORT PathSearch::PathSearch()
+		//:open3(isGreaterThan)
+		{
+
+		}
+		DLLEXPORT PathSearch::~PathSearch()
+		{
+			/*if (searchGraph!=nullptr) {
+				delete searchGraph;
+			}
+			
+			delete gCosts;
+			delete wholeCosts;
+			delete insideOpen;
+			delete parents;*/
+			delete pqUCS;
+			delete visited;
+			delete solution;
+		}
+
+		DLLEXPORT void PathSearch::load(TileMap* _tileMap)
+		{
+			tMap = _tileMap;
+			searchGraph = new unordered_map<Tile*, vector<Tile*>, MyHashFunction>;
+			
+			gCosts = new unordered_map<Tile*, double, MyHashFunction>;
+			wholeCosts = new unordered_map<Tile*, double, MyHashFunction>;
+			insideOpen = new unordered_map<Tile*, int, MyHashFunction>;
+			//parents = new unordered_map<Tile*, pair<int, int>, MyHashFunction>;
+			parents2 = new unordered_map<Tile*, Tile*, MyHashFunction>;
+
+			for (int i = 0; i < _tileMap->getRowCount();i++) {
+				for (int j = 0; j < _tileMap->getColumnCount();j++) {
+					Tile* curr = _tileMap->getTile(i, j);
+					if (curr != nullptr && curr->getWeight()!=0) {
+						(*gCosts)[curr] = 0;
+						//(*hCosts)[curr] = 0;
+						(*wholeCosts)[curr] = 0;
+						(*insideOpen)[curr] = 0;
+						//(*parents)[curr] = make_pair(-1, -1);
+						(*parents2)[curr] = nullptr;
+						if (curr->getWeight() != 0) {
+							vector<Tile*> neighbors;
+							if ((curr->getRow()) % 2 == 0) {
+								if (_tileMap->getTile(i - 1, j) != nullptr && _tileMap->getTile(i - 1, j)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i - 1, j));
+								}
+								if (_tileMap->getTile(i, j - 1) != nullptr && _tileMap->getTile(i, j - 1)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i, j - 1));
+								}
+								if (_tileMap->getTile(i + 1, j) != nullptr && _tileMap->getTile(i + 1, j)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i + 1, j));
+								}
+								if (_tileMap->getTile(i, j + 1) != nullptr && _tileMap->getTile(i, j + 1)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i, j + 1));
+								}
+								if (_tileMap->getTile(i - 1, j - 1) != nullptr && _tileMap->getTile(i - 1, j - 1)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i - 1, j - 1));
+								}
+								if (_tileMap->getTile(i + 1, j - 1) != nullptr && _tileMap->getTile(i + 1, j - 1)->getWeight() != 0)
+									neighbors.push_back(_tileMap->getTile(i + 1, j - 1));
+								//(*searchGraph)[_tileMap->getTile(i, j)] = neighbors;
+								(*searchGraph).insert(make_pair(_tileMap->getTile(i, j), neighbors));
+							}
+							else if ((curr->getRow()) % 2 != 0) {
+								if (_tileMap->getTile(i - 1, j) != nullptr && _tileMap->getTile(i - 1, j)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i - 1, j));
+								}
+								if (_tileMap->getTile(i, j - 1) != nullptr && _tileMap->getTile(i, j - 1)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i, j - 1));
+								}
+								if (_tileMap->getTile(i + 1, j) != nullptr && _tileMap->getTile(i + 1, j)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i + 1, j));
+								}
+								if (_tileMap->getTile(i, j + 1) != nullptr && _tileMap->getTile(i, j + 1)->getWeight() != 0) {
+									neighbors.push_back(_tileMap->getTile(i, j + 1));
+								}
+								if (_tileMap->getTile(i + 1, j + 1) != nullptr && _tileMap->getTile(i + 1, j + 1)->getWeight() != 0)
+									neighbors.push_back(_tileMap->getTile(i + 1, j + 1));
+								if (_tileMap->getTile(i - 1, j + 1) != nullptr && _tileMap->getTile(i - 1, j + 1)->getWeight() != 0)
+									neighbors.push_back(_tileMap->getTile(i - 1, j + 1));
+								//(*searchGraph)[_tileMap->getTile(i, j)] = neighbors;
+								(*searchGraph).insert(make_pair(_tileMap->getTile(i, j), neighbors));
+							}
+						}
+					}
+				}
+			}
+		}
+		DLLEXPORT void PathSearch::initialize(int startRow, int startCol, int goalRow, int goalCol)
+		{
+			done = false;
+			gTile = tMap->getTile(goalRow, goalCol);
+			sTile = tMap->getTile(startRow, startCol);
+
+			visited = new vector<Tile*>;
+			solution = new vector<Tile const*>;
+			/*CompareHeuristic comp((tMap->getTile(goalRow, goalCol))->getXCoordinate(), (tMap->getTile(goalRow, goalCol))->getYCoordinate(), scale);
+			priority_queue<Tile*, vector<Tile*>, CompareHeuristic> pq2(comp);
+			pq = pq2;*/
+			pqUCS = new priority_queue<pair<double, Tile*>>;
+			pqUCS->push(make_pair(0, tMap->getTile(startRow, startCol)));
+			//insideOpen->push_back(tMap->getTile(startRow, startCol));
+			visited->push_back(tMap->getTile(startRow, startCol));
+			//open.push_back(tMap->getTile(startRow, startCol));
+		}
+		DLLEXPORT void PathSearch::update(long timeslice)
+		{
+			if (timeslice == 0) {
+				//while loop in file
+				while (pqUCS->size() != 0) {
+					//while loop in file
+					pair<double, Tile*> p = pqUCS->top();
+					Tile* current = p.second; //acts as queue
+					//p.first = p.first * -1;
+					pqUCS->pop();
+					(*insideOpen)[current] = 0;
+					if (current == gTile) {
+						done = true;
+						//(*parents)[current] = make_pair(current->getRow(), current->getColumn());
+						return;
+					}
+					for (int i = 0; i < (*searchGraph)[current].size();i++) {
+						double newCosts = gCosts->at(current) + (*searchGraph)[current].at(i)->getWeight();
+						if ((*parents2)[(*searchGraph)[current].at(i)] == nullptr)
+							(*parents2)[(*searchGraph)[current].at(i)] = current;
+						if (find(visited->begin(), visited->end(), (*searchGraph)[current].at(i)) == visited->end()) {
+							double fHeuristic = distance(gTile->getXCoordinate(), gTile->getYCoordinate(), (*searchGraph)[current].at(i));
+							(*gCosts)[(*searchGraph)[current].at(i)] = newCosts;
+							visited->push_back((*searchGraph)[current].at(i));
+							double totalCosts = fHeuristic + newCosts * tMap->getTileRadius() * 2 * 1.3;
+							(*wholeCosts)[(*searchGraph)[current].at(i)] = totalCosts;
+							pqUCS->push(make_pair(totalCosts * -1, (*searchGraph)[current].at(i)));
+							(*insideOpen)[(*searchGraph)[current].at(i)] = 1;
+						}
+						else {
+							if (newCosts < gCosts->at((*searchGraph)[current].at(i))) {
+								(*gCosts)[(*searchGraph)[current].at(i)] = newCosts;
+								double totalCosts = distance(gTile->getXCoordinate(), gTile->getYCoordinate(), (*searchGraph)[current].at(i)) + tMap->getTileRadius() * 2 * newCosts * 1.3;
+								(*wholeCosts)[(*searchGraph)[current].at(i)] = totalCosts;
+								//(*parents)[(*searchGraph)[current].at(i)] = make_pair(current->getRow(), current->getColumn());
+								(*parents2)[(*searchGraph)[current].at(i)] = current;
+								if ((*insideOpen)[(*searchGraph)[current].at(i)] == 0) {
+									pqUCS->push(make_pair(totalCosts * -1, (*searchGraph)[current].at(i)));
+									(*insideOpen)[(*searchGraph)[current].at(i)] = 1;
+								}
+							}
+						}
+					}
+				}
+			}
+			else {
+				//https://stackoverflow.com/questions/946167/what-is-the-best-way-to-exit-out-of-a-loop-after-an-elapsed-time-of-30ms-in-c
+				clock_t endwait;
+				endwait = clock() + (timeslice * 1000) * CLOCKS_PER_SEC;
+				while (clock() < endwait && pqUCS->size()!=0)
+				{
+					//while loop in file
+					pair<double, Tile*> p = pqUCS->top();
+					Tile* current = p.second; //acts as queue
+					//p.first = p.first * -1;
+					pqUCS->pop();
+					(*insideOpen)[current] = 0;
+					if (current == gTile) {
+						done = true;
+						//(*parents)[current] = make_pair(current->getRow(), current->getColumn());
+						return;
+					}
+					for (int i = 0; i < (*searchGraph)[current].size();i++) {
+						double newCosts = gCosts->at(current) + (*searchGraph)[current].at(i)->getWeight();
+						if((*parents2)[(*searchGraph)[current].at(i)] == nullptr)
+							(*parents2)[(*searchGraph)[current].at(i)] = current;
+						if (find(visited->begin(), visited->end(), (*searchGraph)[current].at(i)) == visited->end()) {
+							double fHeuristic = distance(gTile->getXCoordinate(), gTile->getYCoordinate(), (*searchGraph)[current].at(i));
+							(*gCosts)[(*searchGraph)[current].at(i)] = newCosts;
+							visited->push_back((*searchGraph)[current].at(i));
+							double totalCosts = fHeuristic + newCosts * tMap->getTileRadius() * 2*1.3;
+							(*wholeCosts)[(*searchGraph)[current].at(i)] = totalCosts;
+							pqUCS->push(make_pair(totalCosts* -1, (*searchGraph)[current].at(i)));
+							(*insideOpen)[(*searchGraph)[current].at(i)] = 1;
+						}
+						else {
+							if (newCosts < gCosts->at((*searchGraph)[current].at(i))) {
+								(*gCosts)[(*searchGraph)[current].at(i)] = newCosts;
+								double totalCosts = distance(gTile->getXCoordinate(), gTile->getYCoordinate(), (*searchGraph)[current].at(i)) + tMap->getTileRadius() * 2* newCosts*1.3;
+								(*wholeCosts)[(*searchGraph)[current].at(i)] = totalCosts;
+								//(*parents)[(*searchGraph)[current].at(i)] = make_pair(current->getRow(), current->getColumn());
+								(*parents2)[(*searchGraph)[current].at(i)] = current;	
+								if ((*insideOpen)[(*searchGraph)[current].at(i)] == 0 ) {
+									pqUCS->push(make_pair(totalCosts * -1, (*searchGraph)[current].at(i)));
+									(*insideOpen)[(*searchGraph)[current].at(i)] = 1;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		DLLEXPORT void PathSearch::shutdown()
+		{
+			delete visited;
+			delete solution;
+			delete pqUCS;
+			for (int i = 0; i < tMap->getRowCount();i++) {
+				for (int j = 0; j < tMap->getColumnCount();j++) {
+					Tile* curr = tMap->getTile(i, j);
+					(*gCosts)[curr] = 0;
+					//(*hCosts)[curr] = 0;
+					(*wholeCosts)[curr] = 0;
+					(*insideOpen)[curr] = 0;
+					(*parents2)[curr] = nullptr;
+					//(*parents)[curr] = make_pair(-1, -1);
+				}
+			}
+			/*CompareHeuristic comp((tMap->getTile(goalRow, goalCol))->getXCoordinate(), (tMap->getTile(goalRow, goalCol))->getYCoordinate(), scale);
+			priority_queue<Tile*, vector<Tile*>, CompareHeuristic> pq2(comp);
+			pq = pq2;*/
+		}
+		DLLEXPORT void PathSearch::unload()
+		{
+			delete searchGraph;
+			delete gCosts;
+			delete wholeCosts;
+			delete insideOpen;
+			//delete parents;
+			delete parents2;
+		}
+		DLLEXPORT bool PathSearch::isDone() const
+		{
+			return done;
+		}
+		DLLEXPORT std::vector<Tile const*> const PathSearch::getSolution() const
+		{
+			int count1 = 0;
+			int count2 = 0;
+			solution->push_back(gTile);
+			Tile* current = gTile;
+
+			while (current != sTile) {
+				solution->push_back((*parents2)[current]);
+				current = (*parents2)[current];
+			}
+			//while (current != sTile) {
+			//	pair<int, int> p = (*parents)[current];;
+			//	if (p.first == -1) {
+			//		for (int k = 0; k < open2->size();k++) {
+			//			if (find((*searchGraph)[current].begin(), (*searchGraph)[current].end(), open2->at(k)) != (*searchGraph)[current].end()) {
+			//				current = open2->at(k);
+			//				//current->setFill(0x7F00FF00);
+			//				count1++;
+			//				solution->push_back(current);
+			//				break;
+			//			}
+			//		}
+			//	}
+			//	else {
+			//		current = tMap->getTile(p.first, p.second);
+			//		count2++;
+			//		solution->push_back(current);
+			//	}
+			//}
+
+			//while (current != sTile) {
+			//	pair<int,int> p = (*parents)[current];
+			//	if (p.first == -1) {
+			//		for (int k = 0; k < visited->size();k++) {
+			//			if (find((*searchGraph)[current].begin(), (*searchGraph)[current].end(), visited->at(k)) != (*searchGraph)[current].end()) {
+			//				current = visited->at(k);
+			//				//current->setFill(0x7F00FF00);
+			//				count1++;
+			//				solution->push_back(current);
+			//				break;
+			//			}
+			//		}
+			//	}
+			//	else {
+			//		count2++;
+			//		current = tMap->getTile(p.first, p.second);
+			//		solution->push_back(current);
+			//	}
+			//}
+			return (*solution);
+		}
+
+	}
+}  // close namespace ufl_cap4053::searches
---
+++ PathSearch/PathSearch.h
@@ -0,0 +1,119 @@
+#pragma once
+#include "../platform.h" // This file will make exporting DLL symbols simpler for students.
+#include "../Framework/TileSystem/Tile.h"
+#include "../Framework/TileSystem/TileMap.h"
+#include <map>
+#include <vector>
+#include <utility>
+#include <unordered_map>
+#include <ctime>
+#include <algorithm>
+#include <list>
+#include <queue>
+#include <iostream>
+//#include "../PriorityQueue.h"
+
+using namespace std;
+namespace ufl_cap4053
+{
+	namespace searches
+	{
+		class PathSearch
+		{
+		// CLASS DECLARATION GOES HERE
+			private:
+				bool done;
+				//double scale;
+				Tile* sTile;
+				Tile* gTile;
+				TileMap* tMap;
+				//https://www.geeksforgeeks.org/how-to-create-an-unordered_map-of-user-defined-class-in-cpp/
+				//https://stackoverflow.com/questions/2634690/good-hash-function-for-a-2d-index
+				class MyHashFunction {
+					public:
+						size_t operator()(const Tile* t) const
+						{
+							return 53* t->getXCoordinate() + 53*t->getYCoordinate();
+						}
+				};
+				//https://www.geeksforgeeks.org/stl-priority-queue-for-structure-or-class/
+				//https://stackoverflow.com/questions/15325730/priority-queue-of-custom-class-using-comparator-with-argument-on-constructor-in
+				//class CompareHeuristic {
+				//public:
+				//	double goalCoordinateX;
+				//	double goalCoordinateY;
+				//	double scale;
+				//	CompareHeuristic() {
+				//		goalCoordinateX = 0;
+				//		goalCoordinateY = 0;
+				//		scale = 0;
+				//	};
+				//	CompareHeuristic(double x, double y, double s) {
+				//		goalCoordinateX = x;
+				//		goalCoordinateY = y;
+				//		scale = s;
+				//	};
+				//	/*CompareHeuristic(double& x, double& y) {
+				//		goalCoordinateX = x;
+				//		goalCoordinateY = y;
+				//	};*/
+				//	bool operator()(Tile* const& p1, Tile* const& p2)
+				//	{
+				//		//int x1 = p1->getRow();
+				//		//int y1 = p1->getColumn();
+				//		//int x2 = p2->getRow();
+				//		//int y2 = p2->getColumn();
+				//		//double distance = sqrt(pow(x1 - goalCoordinateX, 2) + pow(y1 - goalCoordinateY, 2));
+				//		//double distance2 = sqrt(pow(x2 - goalCoordinateX, 2) + pow(y2 - goalCoordinateY, 2));
+				//		////greater than to get least to greatest
+				//		//return (distance - 50 * p1->getWeight()) > (distance2 - 50 * p2->getWeight());
+				//		double x1 = p1->getXCoordinate();
+				//		double y1 = p1->getYCoordinate();
+				//		double x2 = p2->getXCoordinate();
+				//		double y2 = p2->getYCoordinate();
+				//		double distance = sqrt(pow(x1 - goalCoordinateX, 2) + pow(y1 - goalCoordinateY, 2));
+				//		double distance2 = sqrt(pow(x2 - goalCoordinateX, 2) + pow(y2 - goalCoordinateY, 2));
+				//		//greater than to get least to greatest
+				//		return (distance + 0/2*p1->getWeight()) > (distance2 + 0/2*p2->getWeight());
+				//		//return (distance) < (distance2);
+
+				//	}
+				//};
+				double distance(double x, double y, Tile* t) {
+					double x1 = t->getXCoordinate();
+					double y1 = t->getYCoordinate();
+					double distance = sqrt(pow(x1 - x, 2) + pow(y1 - y, 2));
+					return distance;
+				}
+				unordered_map<Tile*, vector<Tile*>, MyHashFunction>* searchGraph;
+				unordered_map<Tile*, double, MyHashFunction>* gCosts;
+				//unordered_map<Tile*, double, MyHashFunction>* hCosts;
+				unordered_map<Tile*, double, MyHashFunction>* wholeCosts;
+				//unordered_map<Tile*, pair<int,int>, MyHashFunction>* parents;
+				unordered_map<Tile*,Tile*, MyHashFunction>* parents2;
+				unordered_map<Tile*, int, MyHashFunction>* insideOpen;
+				//unordered_map<Tile*, int, MyHashFunction>* cost;
+				//priority_queue<Tile*, vector<Tile*>, CompareHeuristic> pq;
+				priority_queue<pair<double, Tile*>>* pqUCS;
+				//priority_queue<Tile*, vector<Tile*>, CompareHeuristic> copyForSolution;
+				//deque<Tile*>* copyForSolution;
+				vector<Tile const*>* solution;
+				vector <Tile*>* visited;
+				//vector <Tile*>* insideOpen;
+				//list <Tile*> open;
+				//vector <Tile*>* open2;
+			public:
+				DLLEXPORT PathSearch(); // EX: DLLEXPORT required for public methods - see platform.h
+				DLLEXPORT ~PathSearch(); //The destructor should perform any final cleanup required before deletion of the object.
+				DLLEXPORT void load(TileMap* _tileMap); //Called after the tile map is loaded.This is usually where the search graph is generated.
+				DLLEXPORT void initialize(int startRow, int startCol, int goalRow, int goalCol); //Called before any update of the path planner; should prepare for search to be performed between the tiles at the coordinates indicated.
+				DLLEXPORT void update(long timeslice); // Called to allow the path planner to execute for the specified timeslice(in milliseconds).Within this method the search should be performed until the time expires or the solution is found.If timeslice is zero(0), this method should only do a single iteration of the algorithm.Otherwise the update should only iterate for the indicated number of milliseconds.
+				DLLEXPORT void shutdown(); // Called when the current search data is no longer needed.It should clean up any memory allocated for this search.Note that this is not the same as the destructor, as the search object may be reset to perform another search on the same map.
+				DLLEXPORT void unload();// Called when the tile map is unloaded.It should clean up any memory allocated for this tile map.Note that this is not the same as the destructor, as the search object may be reinitialized with a new map.
+				DLLEXPORT bool isDone() const; //const Returns true if the update function has finished because it found a solution, and false otherwise.
+				DLLEXPORT std::vector<Tile const*> const getSolution() const;
+		};
+	}
+}  // close namespace ufl_cap4053::searches
+
+
